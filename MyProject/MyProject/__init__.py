9c4f5da188079ee9e65f220032a6764471dc5a0e
MuraliGowda <murali.apqa@gmail.com>
2018-09-12 11:04:04 +0530

    Initial staructure of DJANGO Framework

---
+++ MyProject/MyProject/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for MyProject.
+"""
---
+++ MyProject/PYTHONPATH/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\Desktop\MSO365 Stencil\MyProject\MyProject\PYTHONPATH\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
